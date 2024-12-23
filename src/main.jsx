f5207fb03f565d0f13e3e1614e617cee54f0fc23
Ayush Dangol <meiayush@gmail.com>
2024-12-23 06:08:28 -0500

    added portfolio in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import Portfolio from "./components/portfolio";
 
 function Home() {
   return <h1>Welcome to the Home Page</h1>;
@@ -7,5 +8,6 @@ function Home() {
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Home />
+    <Portfolio />
   </StrictMode>
 );
